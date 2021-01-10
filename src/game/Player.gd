3a738856649cdae71f262a1d7976177de12365f3
Willowblade <laurentvanacker@gmail.com>
2021-01-10 03:47:22 +0100

    Added level and moveable player. Automatic variant selection to fit collisions

---
+++ src/game/Player.gd
@@ -0,0 +1,95 @@
+extends KinematicBody2D
+class_name Player
+
+onready var sprite: AnimatedSprite = $Sprite
+
+
+signal interact
+
+var direction = Vector2(1, 0)
+
+const MOVEMENT_SPEED: float = 50.0
+
+var state = "IDLE"
+
+func _ready():
+	pass
+
+
+func set_flip(velocity: Vector2):
+	if velocity.x > 0:
+		if sprite.flip_h == false:
+			sprite.flip_h = true
+
+	elif velocity.x < 0:
+		if sprite.flip_h == true:
+			sprite.flip_h = false
+			
+func set_animation(velocity: Vector2):	
+	if velocity.x < 0:
+		sprite.animation = "walk"
+		if state != "HORIZONTAL_LEFT":
+			state = "HORIZONTAL_LEFT"
+	elif velocity.x > 0:
+		sprite.animation = "walk"
+		if state != "HORIZONTAL_RIGHT":
+			state = "HORIZONTAL_RIGHT"
+		
+	elif velocity.y < 0:
+		sprite.animation = "walk"
+		if state != "VERTICAL_UP":
+			state = "VERTICAL_UP"
+
+	elif velocity.y > 0:
+		sprite.animation = "walk"
+		if state != "VERTICAL_DOWN":
+			state = "VERTICAL_DOWN"
+	else:
+		state = "IDLE"
+		
+
+func set_direction(velocity: Vector2):
+	if velocity.x > 0:
+		direction = Vector2(1, 0)
+	elif velocity.x < 0:
+		direction = Vector2(-1, 0)
+	elif velocity.y < 0:
+		direction = Vector2(0, -1)
+	elif velocity.y > 0:
+		direction = Vector2(0, 1)
+	
+func player_specific(delta):
+	pass
+			
+func _physics_process(delta):
+	player_specific(delta)
+
+	
+	var velocity = Vector2(0, 0)
+	var speed_factor = 1.0
+	
+	if Input.is_action_pressed("move_left"):
+		velocity.x -= MOVEMENT_SPEED * speed_factor
+		
+	if Input.is_action_pressed("move_right"):
+		velocity.x += MOVEMENT_SPEED * speed_factor
+		
+	if Input.is_action_pressed("move_down"):
+		velocity.y += MOVEMENT_SPEED * speed_factor
+		
+	if Input.is_action_pressed("move_up"):
+		velocity.y -= MOVEMENT_SPEED * speed_factor
+		
+	if Input.is_action_just_pressed("interact"):
+		emit_signal("interact")
+		
+		
+	if Input.is_action_just_pressed("debugging"):
+		# GameState.grow()
+		pass
+		
+	set_flip(velocity)
+	set_animation(velocity)
+	set_direction(velocity)
+	
+	move_and_collide(velocity * delta)
---
+++ src/game/level/floor/Floor.gd
@@ -0,0 +1,113 @@
+extends TileMap
+
+
+var name_to_id_mapping = {}
+var id_to_name_mapping = {}
+
+var floor_border = {}
+
+func _ready():
+	var ids = tile_set.get_tiles_ids()
+	for id in ids:
+		var tile_name = tile_set.tile_get_name(id)
+		name_to_id_mapping[tile_name] = id
+		id_to_name_mapping[id] = tile_name
+	print(name_to_id_mapping)
+	print(id_to_name_mapping)
+	clean()
+	
+func clean():
+	var used_cells = {}
+	for cellv in get_used_cells():
+		used_cells[cellv] = get_cellv(cellv)
+	
+	for used_cell in used_cells.keys():
+		var cell_name: String = id_to_name_mapping[used_cells[used_cell]]
+		var nw = used_cell + Vector2(-1, 0)
+		var ne = used_cell + Vector2(0, -1)
+		var sw = used_cell + Vector2(0, 1)
+		var se = used_cell + Vector2(1, 0)
+		var nw_exists = used_cells.has(nw)
+		var ne_exists = used_cells.has(ne)
+		var sw_exists = used_cells.has(sw)
+		var se_exists = used_cells.has(se)
+		if cell_name.begins_with("floor"):
+			if !sw_exists:
+				floor_border[sw] = "sw"
+			if !nw_exists:
+				floor_border[nw] = "nw"
+			if !ne_exists:
+				floor_border[ne] = "ne"
+			if !se_exists:
+				floor_border[se] = "se"
+				
+			if !sw_exists and !se_exists:
+				set_cellv(used_cell, name_to_id_mapping["floor_half"])
+			elif !sw_exists:
+				if used_cells.has(used_cell + Vector2(1, 1)) and id_to_name_mapping[used_cells[used_cell + Vector2(1, 1)]].begins_with("floor"):
+					set_cellv(used_cell, name_to_id_mapping["floor_half_left_obstructed"])
+				elif used_cells.has(used_cell + Vector2(1, 1)) and id_to_name_mapping[used_cells[used_cell + Vector2(1, 1)]] == "slope_sw":
+					set_cellv(used_cell, name_to_id_mapping["floor_half_left_obstructed_slope"])
+				else:
+					set_cellv(used_cell, name_to_id_mapping["floor_half_left"])
+			elif !se_exists:
+				if used_cells.has(used_cell + Vector2(1, 1)) and id_to_name_mapping[used_cells[used_cell + Vector2(1, 1)]].begins_with("floor"):
+					set_cellv(used_cell, name_to_id_mapping["floor_half_right_obstructed"])
+				elif used_cells.has(used_cell + Vector2(1, 1)) and id_to_name_mapping[used_cells[used_cell + Vector2(1, 1)]] == "slope_se":
+					set_cellv(used_cell, name_to_id_mapping["floor_half_right_obstructed_slope"])
+				else:
+					set_cellv(used_cell, name_to_id_mapping["floor_half_right"])
+				
+			elif sw_exists and id_to_name_mapping[used_cells[sw]].begins_with("slope_nw"):
+					set_cellv(used_cell, name_to_id_mapping["floor_half_left_slope_nw"])
+			elif se_exists and id_to_name_mapping[used_cells[se]].begins_with("slope_ne"):
+					set_cellv(used_cell, name_to_id_mapping["floor_half_right_slope_ne"])
+			else:
+				set_cellv(used_cell, name_to_id_mapping["floor"])
+				
+				
+		if cell_name.begins_with("slope"):
+			if cell_name.begins_with("slope_sw"):
+				if se_exists:
+					set_cellv(used_cell, name_to_id_mapping["slope_sw_obstructed"])
+					if nw_exists and id_to_name_mapping[used_cells[nw]].begins_with("slope_sw"):
+						set_cellv(used_cell, name_to_id_mapping["slope_sw_obstructed_middle"])
+				else:
+					if nw_exists and id_to_name_mapping[used_cells[nw]].begins_with("slope_sw"):
+						set_cellv(used_cell, name_to_id_mapping["slope_sw_begin"])
+			if cell_name.begins_with("slope_se"):
+				if sw_exists:
+					set_cellv(used_cell, name_to_id_mapping["slope_se_obstructed"])
+					if ne_exists and id_to_name_mapping[used_cells[ne]].begins_with("slope_se"):
+						set_cellv(used_cell, name_to_id_mapping["slope_se_obstructed_middle"])
+				else:
+					if ne_exists and id_to_name_mapping[used_cells[ne]].begins_with("slope_se"):
+						set_cellv(used_cell, name_to_id_mapping["slope_se_begin"])
+			if cell_name.begins_with("slope_nw"):
+				if sw_exists:
+					set_cellv(used_cell, name_to_id_mapping["slope_nw_obstructed"])
+					if ne_exists and id_to_name_mapping[used_cells[ne]].begins_with("slope_nw"):
+						set_cellv(used_cell, name_to_id_mapping["slope_nw_obstructed_middle"])
+				else:
+					if used_cells.has(sw + Vector2(1, 0)) and id_to_name_mapping[used_cells[sw + Vector2(1, 0)]].begins_with("floor"):
+						set_cellv(used_cell, name_to_id_mapping["slope_nw_half_obstructed"])
+						if ne_exists and id_to_name_mapping[used_cells[ne]].begins_with("slope_nw"):
+							set_cellv(used_cell, name_to_id_mapping["slope_nw_half_obstructed_begin"])
+					else:
+						set_cellv(used_cell, name_to_id_mapping["slope_nw"])
+						if ne_exists and id_to_name_mapping[used_cells[ne]].begins_with("slope_nw"):
+							set_cellv(used_cell, name_to_id_mapping["slope_nw_begin"])
+			if cell_name.begins_with("slope_ne"):
+				if se_exists:
+					set_cellv(used_cell, name_to_id_mapping["slope_ne_obstructed"])
+					if nw_exists and id_to_name_mapping[used_cells[nw]].begins_with("slope_ne"):
+						set_cellv(used_cell, name_to_id_mapping["slope_ne_obstructed_middle"])
+				else:
+					if used_cells.has(se + Vector2(0, 1)) and id_to_name_mapping[used_cells[se + Vector2(0, 1)]].begins_with("floor"):
+						set_cellv(used_cell, name_to_id_mapping["slope_ne_half_obstructed"])
+						if nw_exists and id_to_name_mapping[used_cells[nw]].begins_with("slope_ne"):
+							set_cellv(used_cell, name_to_id_mapping["slope_ne_half_obstructed_begin"])
+					else:
+						set_cellv(used_cell, name_to_id_mapping["slope_ne"])
+						if nw_exists and id_to_name_mapping[used_cells[nw]].begins_with("slope_ne"):
+							set_cellv(used_cell, name_to_id_mapping["slope_ne_begin"])
